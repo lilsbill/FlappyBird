81709b6acc0cb857121f2f524fd0f4ec7c8a480e
kaitlynsamuelian <kaitlyn.samuelian@colorado.edu>
2024-04-17 09:45:33 -0600

    Update menuScreen.py

--- menuScreen.py
+++ menuScreen.py
@@ -1,18 +1,23 @@
 # menu screen set up
 import pygame
 pygame.init()
+
+#give some basics for the screen setup
 WHITE= (255, 255, 255)
 BLUE= (0, 0, 255)
 SCREEN_WIDTH=800
 SCREEN_HEIGHT=600
 
+#set up screen
 SCREEN=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Main Menu")
 
-#fonts
+#sets up font 
 font = pygame.font.SysFont("arialblack", 40)
+#sets color of text to blue
 TEXT_COL = (BLUE)
 
+#function for the text on the screen
 def draw_text(text, font, TEXT_COL, x, y):
     img= font.render(text, True, TEXT_COL)
     SCREEN.blit(img, (x, y))
